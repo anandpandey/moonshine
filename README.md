46dade96a1081cb6a4317a54f351c8c6eef3d5b2
Anand Pandey <anand.etalk@gmail.com>
2015-01-16 21:02:40 +0530

    - Moved Fragment to a separate class - Created menu for options

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # moonshine
 Android App - Practice for Android course on udacity
+
+# Opengraph API URL
+http://api.openweathermap.org/data/2.5/forecast?q=400068&mode=json&units=metric&cnt=7
--- app/src/main/java/com/example/anand/moonshine/MainActivity.java
+++ app/src/main/java/com/example/anand/moonshine/MainActivity.java
@@ -1,18 +1,9 @@
 package com.example.anand.moonshine;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -23,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -33,7 +24,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -51,33 +42,4 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            String[] arrayOfData = {
-                    "Today - Sunny - 88/63", "Tomorrow - Foggy - 70/46",
-                    "Weds - Cloudy - 72/63", "Thurs - Rainy - 64/51",
-                    "Fri - Foggy - 70/46", "Sat - Sunny - 76/68", "Sun - Sunny - 77/63"};
-
-            List<String> weeklyForecastData = Arrays.asList(arrayOfData);
-
-
-            ArrayAdapter<String> forecastDataAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weeklyForecastData);
-
-            ListView listView = (ListView) rootView.findViewById(R.id.list_item_forecast_textview);
-            listView.setAdapter(forecastDataAdapter);
-
-            return rootView;
-        }
-    }
 }
